7a601335a6ee02eaf0229334c2ccf7dab0d1af58
Kalhara JA <kalhara.ja@gmail.com>
2024-05-16 01:02:14 +0530

    fix: fix no css issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: 'https://enlist-form.gutricious.com/en',
+};
  
 export default withNextIntl(nextConfig);
