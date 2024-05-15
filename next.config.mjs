ed27e5eecdae441ecfbe1a2c16b771a46047684b
Kalhara JA <kalhara.ja@gmail.com>
2024-05-16 01:18:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: 'https://enlist-form.gutricious.com/en',
 };
- 
+
 export default withNextIntl(nextConfig);
