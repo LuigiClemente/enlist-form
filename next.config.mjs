d3dc642af17a3b6913be9bb167f997545c336a07
Kalhara JA <kalhara.ja@gmail.com>
2024-05-17 14:36:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/enlist-form',
     assetPrefix: 'https://enlist-form.gutricious.com/',
     images: {
         remotePatterns: [
