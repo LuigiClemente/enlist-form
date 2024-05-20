777236983f1bcb81d12b4e20320044501e0f676f
Kalhara JA <kalhara.ja@gmail.com>
2024-05-20 18:03:01 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,30 @@
+import { routes } from '@/utils/routes';
+import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
-import { languages, locales } from './utils/languages';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales,
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
-});
- 
+
+
+export function middleware(request: NextRequest) {
+    const url = request.nextUrl.clone();
+    
+    // Dynamically handle redirection based on the pathname
+    for (const locale in routes) {
+        for (const key in routes[locale]) {
+            if (url.pathname === routes[locale][key]) {
+                url.pathname = `/${locale}`; // Redirect to the root of the locale
+                break;
+            }
+        }
+    }
+
+    // Handle internationalization
+    const handleI18n = createMiddleware({
+        locales: ['en', 'de', 'fr', 'es', 'it', 'nl', 'pt'],
+        defaultLocale: 'en'
+    });
+
+    return handleI18n(new NextRequest(url, request));
+}
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|pt|es|fr|it|nl|de)/:path*']
+    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'] // Ensuring the matcher excludes specific paths
 };
